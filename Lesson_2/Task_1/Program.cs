b8e5e4ec0ea7cc16d4d7b4642c09ba111c629ec8
RecloserSC <roz_rs@mail.ru>
2018-11-27 02:00:56 +0400

    Сделал 2.5 задания + ignore

---
+++ Lesson_2/Task_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task_1
+{
+    // Роман Р
+
+    //Написать метод, возвращающий минимальное из трёх чисел.
+    class Program
+    {
+        static int min()
+        {
+            Console.WriteLine("             << min_number >>");
+            Console.WriteLine("");
+            Console.WriteLine("Вычисление минимального числа");
+            Console.WriteLine("");
+
+            Console.Write("Введите первое число:  ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите второе число:  ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите третье число:  ");
+            int n3 = Convert.ToInt32(Console.ReadLine());
+
+            int n;
+            if (n1 < n2)
+
+                n = n1;
+
+            else
+
+                n = n2;
+
+            if (n3 < n)
+
+                n = n3;
+
+            Console.WriteLine("");
+            Console.Write("Минимальное число: " + n);
+            return n1;
+        }
+        static void Main(string[] args)
+        {
+            min();
+            //Console.WriteLine("мин"+ min());
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Lesson_2/Task_3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task_3
+{
+    //Роман Р
+
+    //С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечётных положительных чисел.
+
+    class Program
+    {
+        static bool Odd(int a) //метод определения четных или нечетных чисел
+        {
+            return a % 1 == 0; 
+        }
+        static void Main(string[] args) //точка запуска
+        {
+            int n = 1;
+            int x=0;
+            while (n > 0)
+            {                
+                Console.Write("Введите число: ");
+                int m = Convert.ToInt32(Console.ReadLine());
+                
+                if (Odd(m) & m > 0)
+                {
+                    x=x+m;
+ 
+                }
+
+
+                
+                //if (m % 1 == 0)
+                //{
+                //    x = x + m;
+
+                //}
+              
+                Console.WriteLine(x);
+                n = m;
+             
+            }
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
